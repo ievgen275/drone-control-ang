7b79f5e92d140238e82158982e3ea3ee15dfda26
Yevgen <ievgen3435@gmail.com>
2024-07-26 18:43:38 +0300

    start dev in ang

---
+++ src/app/joystick/joystick.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joystick',
+  standalone: true,
+  imports: [],
+  templateUrl: './joystick.component.html',
+  styleUrl: './joystick.component.scss'
+})
+export class JoystickComponent {
+
+}
